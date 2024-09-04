86ebcea7661f8c3942b2d9a37285f63040387d2f
uri <uri.valevski@gmail.com>
2024-09-04 16:58:43 +0200

    support-refernces-in-green-api

--- src/greenApi.ts
+++ src/greenApi.ts
@@ -1,15 +1,17 @@
 import { gamla } from "../deps.ts";
 
-// @ts-expect-error no typing
-import greenApi from "npm:@green-api/whatsapp-api-client";
+import greenApi from "npm:@green-api/whatsapp-api-client@0.4.0-0";
 
 const { pipe } = gamla;
 
 import {
   injectFileLimitMB,
   injectMedium,
+  injectMessageId,
+  injectReferenceId,
   injectReply,
   injectSendFile,
+  injectSpinner,
   injectUserId,
   TaskHandler,
 } from "./api.ts";
@@ -27,11 +29,23 @@ type ExtendedTextMessageData = {
   isForwarded: false;
 };
 
+type MessageResponse = {
+  idMessage: string;
+};
+
 type SenderData = {
   chatId: string;
   chatName: string;
   sender: string;
   senderName: string;
+};
+
+type QuotedMessage = {
+  stanzaId: string;
+  participant: string;
+  typeMessage: string;
+  // More fields according to the type.
+  // See here https://green-api.com/en/docs/api/receiving/notifications-format/.
 };
 
 type GreenApiMessage = {
@@ -42,16 +56,21 @@ type GreenApiMessage = {
     | {
       typeMessage: "textMessage";
       textMessageData: { textMessage: string };
+      quotedMessage?: QuotedMessage;
     }
     | {
       typeMessage: "extendedTextMessage";
       extendedTextMessageData: ExtendedTextMessageData;
+      quotedMessage?: QuotedMessage;
     };
 };
 
 const messageSender = ({
   senderData: { sender },
 }: GreenApiMessage) => sender;
+
+const greenApiReferenceId = (x: GreenApiMessage) =>
+  x.messageData.quotedMessage?.stanzaId;
 
 const messageText = ({ messageData }: GreenApiMessage) =>
   messageData.typeMessage === "extendedTextMessage"
@@ -65,18 +84,22 @@ export const registerWebhook = (
 
 const communications = <T extends TaskHandler>(
   api: ReturnType<typeof greenApi.restAPI>,
+  msgId: string,
+  referenceId: string,
   userId: string,
+  send: (txt: string) => Promise<string>,
 ) =>
   pipe(
     injectMedium(() => "green-api")<T>,
+    injectMessageId(() => msgId)<T>,
     injectFileLimitMB(() => 50)<T>,
     injectUserId(() => userId)<T>,
     injectSendFile((url: string) =>
       api.file.sendFileByUrl(userId, null, url, "video.mp4", "")
     )<T>,
-    injectReply((txt: string) => api.message.sendMessage(userId, null, txt))<
-      T
-    >,
+    injectReferenceId(() => referenceId)<T>,
+    injectSpinner(pipe(send, (_) => () => Promise.resolve()))<T>,
+    injectReply(send)<T>,
   );
 
 export const greenApiHandler = (
@@ -88,7 +111,16 @@ export const greenApiHandler = (
   method: "POST",
   path,
   handler: (msg: GreenApiMessage) =>
-    communications(greenApi.restAPI(credentials), messageSender(msg))(doTask)({
-      text: messageText(msg),
-    }),
+    communications(
+      greenApi.restAPI(credentials),
+      msg.idMessage,
+      greenApiReferenceId(msg) ?? "",
+      messageSender(msg),
+      (txt: string) =>
+        greenApi.restAPI(credentials).message.sendMessage(
+          messageSender(msg),
+          null,
+          txt,
+        ).then(({ idMessage }: MessageResponse) => idMessage),
+    )(doTask)({ text: messageText(msg) }),
 });
