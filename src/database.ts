710adaf139236d9507354961c170ae6f49a43190
uri <uri.valevski@gmail.com>
2024-11-01 16:28:11 +0900

    auth

--- src/database.ts
+++ src/database.ts
@@ -2,18 +2,20 @@ import type { TaskHandler } from "./api.ts";
 import type { Endpoint } from "./taskBouncer.ts";
 import { makeKey, now, websocketInject } from "./websocket.ts";
 
-type ClientRequest = { from: string; text: string };
+type ClientRequest = { from: string; text: string; token: string };
 
 export const makeDatabaseHandler = (
     storer: <T>(value: T) => Promise<void>,
     doTask: TaskHandler,
     path: string,
     botName: string,
+    authenticate: (token: string, userId: string) => Promise<boolean>,
 ): Endpoint => ({
     bounce: true,
     method: "POST",
     path,
-    handler: async ({ from, text }: ClientRequest) => {
+    handler: async ({ from, text, token }: ClientRequest) => {
+        if (!await authenticate(token, from)) return;
         await storer({ from, key: makeKey(), text, time: now() });
         return websocketInject(
             (x) => storer({ ...x, time: now(), from: botName }),
