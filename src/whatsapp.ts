2343f6acdafeeaf94feeabec2bfd9a926820c2b7
Uri <uri.valevski@gmail.com>
2026-02-11 06:20:05 +0200

    disable

--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -20,7 +20,6 @@ import {
 } from "gamla";
 import {
   injectBotPhone,
-  injectEditMessage,
   injectLastEvent,
   injectMedium,
   injectMessageId,
@@ -516,9 +515,6 @@ export const whatsappForBusinessInjectDepsAndRun =
       injectSpinner(pipe(send, (_) => () => Promise.resolve())),
       injectReply(send),
       injectReplyImage(sendImageReply),
-      injectEditMessage((msgId: string, text: string) =>
-        editWhatsappMessage(token, toNumberId(msg))(msgId, text)
-      ),
       injectTyping(() =>
         sendWhatsappTypingIndicator(token, toNumberId(msg))(
           messageId(msg),
@@ -539,24 +535,6 @@ export const whatsappBusinessHandler = (
   predicate: ({ url, method }) => url === path && method === "POST",
   handler: whatsappForBusinessInjectDepsAndRun(token, doTask),
 });
-
-const editWhatsappMessage =
-  (accessToken: string, fromNumberId: string) =>
-  (messageId: string, text: string): Promise<void> =>
-    fetch(
-      `https://graph.facebook.com/${apiVersion}/${fromNumberId}/messages/${messageId}`,
-      {
-        method: "PUT",
-        body: JSON.stringify({
-          messaging_product: "whatsapp",
-          type: "text",
-          text: { body: convertToWhatsAppFormat(text) },
-        }),
-        headers: makeHeaders(accessToken),
-      },
-    ).then(async (response) => {
-      if (!response.ok) throw new Error(await response.text());
-    });
 
 const sendWhatsappTypingIndicator =
   (accessToken: string, fromNumberId: string) => (messageId: string) =>
